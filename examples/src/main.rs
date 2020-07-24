8b058ae3a99fcb9d4a34d822531ef35f62de96eb
Yvan Sraka <yvan@sraka.xyz>
2020-07-24 16:30:55 +0200

    Feedbacks

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,23 +1,19 @@
 extern crate clap;
-extern crate serde;
-extern crate serde_json;
 extern crate libray;
 extern crate ppm;
+extern crate serde;
+extern crate serde_json;
 
-use clap::{Arg, App};
-use std::fs::{File};
+use clap::{App, Arg};
 use libray::ray_tracing::scene::Scene;
+use std::fs::File;
 use std::path::Path;
 
 fn main() {
     let app = App::new("libray")
         .version("0.1")
-        .arg(Arg::with_name("scene")
-            .required(true)
-            .index(1))
-        .arg(Arg::with_name("image")
-            .required(true)
-            .index(2));
+        .arg(Arg::with_name("scene").required(true).index(1))
+        .arg(Arg::with_name("image").required(true).index(2));
     let matches = app.get_matches();
 
     let scene_path = matches.value_of("scene").unwrap();
